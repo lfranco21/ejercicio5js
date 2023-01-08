fd946ae86542bd6ad898f364948036115602d9a5
Luis Franco <lfranco21@users.noreply.github.com>
2023-01-08 17:56:50 -0500

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,14 @@
+let altura_cm = 175;
+console.log(altura_cm);
+
+let altura_m = 1.75;
+console.log(altura_m.toFixed(1));
+
+let peso_kg = 91.7;
+console.log(peso_kg.toPrecision(2));
+
+let max_valor_js= Number.MAX_VALUE;
+let suma = max_valor_js + 1;
+
+console.log(suma === max_valor_js);
+
